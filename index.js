02e0c38707b2ed2c2844ff8f3f6c42464e26d468
AvielFl <aviel4028@gmail.com>
2021-07-19 12:36:02 -0500

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+const user = document.getElementById('user');
+const mail = document.getElementById('mail');
+const correctOne = document.getElementById('correct');
+const warningOne = document.getElementById('warning');
+const register = document.getElementById('register');
+const correctTwo = document.getElementById('correctTwo');
+const warningTwo = document.getElementById('warningTwo');
